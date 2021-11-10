e7fe72dffaeb42424a26ae449de2c4289cfb84b5
LAPTOP-5PNJLMRU\asus <muhammed.s@deepnetsoft.com>
2021-11-10 19:34:19 +0530

    partials created

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const indexRouter = require('./routes/index')
 const mongoose = require('mongoose')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 if( process.env.NODE_ENV !== 'production' ){
     require('dotenv').config()
@@ -23,6 +24,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
