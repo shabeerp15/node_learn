43aca375f3247b6c8cf95b825574135ab7dff3c7
LAPTOP-5PNJLMRU\asus <muhammed.s@deepnetsoft.com>
2021-11-10 10:22:37 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
+const mongoose = require('mongoose')
+
+if( process.env.NODE_ENV !== 'production' ){
+    require('dotenv').config()
+}
+
+const mongoDB = process.env.MONGODB_URI 
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => {
+    console.log('Connected to Mongoose')
+})
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use('/', indexRouter)
+
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
